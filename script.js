b68e605f8dbf3b6c8b7ece4e36052198bcd145cb
CHRISTOBEAR <christophermanloloyo@gmail.com>
2021-07-25 13:35:59 -1000

    created 3 broadcast buttons

--- script.js
+++ script.js
@@ -83,9 +83,22 @@ function ProcessReceivedMessage(msg){
   }
 }
 
-document.getElementById("broadcast-buttons").addEventListener("submit", SendBNB);
+document.getElementById("bnb").addEventListener("submit", SendBNB);
 function SendBNB(e){
   e.preventDefault();
   // send entire blockchain out to everyone
   console.log(username+": blockchain broadcast");
+}
+
+document.getElementById("nbb").addEventListener("submit", SendNBB);
+function SendNBB(e){
+  e.preventDefault();
+  // send entire blockchain out to everyone
+  console.log(username+": new block broadcast");
+}
+document.getElementById("nbt").addEventListener("submit", SendNBT);
+function SendNBT(e){
+  e.preventDefault();
+  // send entire blockchain out to everyone
+  console.log(username+": transaction broadcast");
 }
